0e02ff18682ca2fc594b5810abbc543af906085d
madhead <siarhei.krukau@gmail.com>
2019-03-26 17:14:47 +0300

    Test Lambda implementation

--- lambda-test/build.gradle.kts
+++ lambda-test/build.gradle.kts
@@ -1,22 +1,28 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
+
 plugins {
     kotlin("jvm").version("1.3.21")
     id("com.github.johnrengelman.shadow").version("5.0.0")
 }
 
 dependencies {
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.11.1"))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.9.8"))
+
     implementation(project(":model"))
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
-    testImplementation("org.skyscreamer:jsonassert")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.apache.logging.log4j:log4j-core")
+    implementation("com.amazonaws:aws-lambda-java-log4j2:1.1.0")
 }
 
 tasks {
     val test by getting(Test::class) {
         useJUnitPlatform()
+    }
+    val shadowJar by getting(ShadowJar::class) {
+        transform(Log4j2PluginsCacheFileTransformer::class.java)
     }
 }
--- lambda-test/src/main/kotlin/by/dev/madhead/utils/lambda_proxy_integration/lambdatest/Handler.kt
+++ lambda-test/src/main/kotlin/by/dev/madhead/utils/lambda_proxy_integration/lambdatest/Handler.kt
@@ -4,13 +4,35 @@ import by.dev.madhead.utils.lambda_proxy_integration.model.ProxyIntegrationReque
 import by.dev.madhead.utils.lambda_proxy_integration.model.ProxyIntegrationResponse
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.logging.log4j.LogManager
+
+private data class Response(
+    val input: ProxyIntegrationRequest,
+    val context: Context
+)
 
 class Handler : RequestHandler<ProxyIntegrationRequest, ProxyIntegrationResponse> {
+    companion object {
+        @JvmStatic
+        private val logger = LogManager.getLogger(Handler::class.java)
+
+        @JvmStatic
+        private val objectMapper = ObjectMapper()
+    }
+
     override fun handleRequest(input: ProxyIntegrationRequest, context: Context): ProxyIntegrationResponse {
+        logger.debug(objectMapper.writeValueAsString(input))
+        logger.debug(objectMapper.writeValueAsString(context))
+
+        val response = Response(input, context)
+
+        logger.debug(objectMapper.writeValueAsString(input))
+
         return ProxyIntegrationResponse(
             200,
             emptyMap(),
-            "Hello, World!"
+            objectMapper.writeValueAsString(response)
         )
     }
 }
